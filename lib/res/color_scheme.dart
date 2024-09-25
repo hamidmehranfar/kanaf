ceba1778c12af0520ed35f1124e796174eb29e9a
hhaa1382 <mehranfarhamid82@gmail.com>
2024-09-25 16:06:21 +0330

    add color sheme

---
+++ lib/res/color_scheme.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+ColorScheme lightColorScheme = const ColorScheme(
+  brightness: Brightness.light,
+  primary: Color(0xFF1976D2),
+  secondary: Color(0xFF00ACC1),
+  surface: Color(0xFFFFFFFF),
+  error: Color(0xFFD32F2F),
+  onPrimary: Color(0xFFFFFFFF),
+  onSecondary: Color(0xFFFFFFFF),
+  onSurface: Color(0xFF000000),
+  onError: Color(0xFFFFFFFF),
+  primaryContainer: Color(0xFFBBDEFB),
+  secondaryContainer: Color(0xFFB2EBF2),
+  onPrimaryContainer: Color(0xFF0D47A1),
+  onSecondaryContainer: Color(0xFF006064),
+);
+
+ColorScheme darkColorScheme = const ColorScheme(
+  brightness: Brightness.dark,
+  primary: Color(0xFF9C27B0),
+  secondary: Color(0xFFFF5722),
+  surface: Color(0xFF424242),
+  error: Color(0xFFCF6679),
+  onPrimary: Color(0xFFFFFFFF),
+  onSecondary: Color(0xFFFFFFFF),
+  onSurface: Color(0xFFFFFFFF),
+  onError: Color(0xFF000000),
+  primaryContainer: Color(0xFFCE93D8),
+  secondaryContainer: Color(0xFFFFCCBC),
+  onPrimaryContainer: Color(0xFF4A0072),
+  onSecondaryContainer: Color(0xFFBF360C),
+);
