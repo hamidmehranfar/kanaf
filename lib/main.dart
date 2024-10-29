6da5f90f1e551347fd18363aaf70a0f4a1609ebb
hhaa1382 <mehranfarhamid82@gmail.com>
2024-10-29 23:21:03 +0330

    add fonts

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
         iconTheme: const IconThemeData(
           size: 20
         ),
-        fontFamily: "IranSans",
+        fontFamily: "YekanBakh",
       ),
       locale: const Locale("fa"),
       home: const MainScreen(),
